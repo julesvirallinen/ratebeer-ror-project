d7d0cdce3958c02aff636aa6442df4944991b8ea
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-19 22:04:46 +0200

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,33 @@ b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1.beers.create name:"Iso 3", style_id:4
-b1.beers.create name:"Karhu", style_id:4
-b1.beers.create name:"Tuplahumala", style_id:4
-b2.beers.create name:"Huvila Pale Ale", style_id:3
-b2.beers.create name:"X Porter", style_id:6
-b3.beers.create name:"Hefeweizen", style_id:5
-b3.beers.create name:"Helles", style_id:4
+s1 = Style.create name:"IPA", description: "Lorem ipsum dolor sit jne"
+s2 = Style.create name:"Lager", description: "Lorem ipsum dolor sit jne"
+s3 = Style.create name:"Weizen", description: "Lorem ipsum dolor sit jne"
+s4 = Style.create name:"Porter", description: "Lorem ipsum dolor sit jne"
+s5 = Style.create name:"Low alcohol", description: "Lorem ipsum dolor sit jne"
+
+
+be1 = Beer.create name:"Iso 3"
+be2 = Beer.create name:"Karhu"
+be3 = Beer.create name:"Tuplahumala"
+be4 = Beer.create name:"Huvila Pale Ale"
+be5 = Beer.create name:"X Porter"
+be6 = Beer.create name:"Hefeweizen"
+be7 = Beer.create name:"Helles"
+
+b1.beers << be1
+b1.beers << be2
+b1.beers<< be3
+b2.beers << be4
+b2.beers << be5
+b3.beers << be6
+b3.beers << be7
+
+s1.beers << be4
+s2.beers << be1
+s2.beers << be2
+s2.beers << be3
+s2.beers << be7
+s3.beers << be6
+s4.beers << be5
