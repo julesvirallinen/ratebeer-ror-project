4d160259bf80a91b43eee9ff72d3c014c7880b3a
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-20 00:03:00 +0200

    Fixed tests for Places

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -40,7 +40,44 @@ describe "Places" do
 end
 
 def search_for_kumpula
+  allow(BeermappingApi).to receive(:weather_in).with("kumpula").and_return(
+      {"location"=>
+           {"name"=>"Helsinki",
+            "region"=>"Southern Finland",
+            "country"=>"Finland",
+            "lat"=>60.18,
+            "lon"=>24.93,
+            "tz_id"=>"Europe/Helsinki",
+            "localtime_epoch"=>1487545523,
+            "localtime"=>"2017-02-19 23:05"},
+       "current"=>
+           {"last_updated_epoch"=>1487545440,
+            "last_updated"=>"2017-02-19 23:04",
+            "temp_c"=>3.0,
+            "temp_f"=>37.4,
+            "is_day"=>0,
+            "condition"=>{"text"=>"Partly cloudy", "icon"=>"//cdn.apixu.com/weather/64x64/night/116.png", "code"=>1003},
+            "wind_mph"=>10.5,
+            "wind_kph"=>16.9,
+            "wind_degree"=>210,
+            "wind_dir"=>"SSW",
+            "pressure_mb"=>991.0,
+            "pressure_in"=>29.7,
+            "precip_mm"=>0.0,
+            "precip_in"=>0.0,
+            "humidity"=>93,
+            "cloud"=>0,
+            "feelslike_c"=>-1.0,
+            "feelslike_f"=>30.2,
+            "vis_km"=>10.0,
+            "vis_miles"=>6.0}}
+
+  )
+
+
   visit places_path
   fill_in('city', with: 'kumpula')
   click_button "Search"
+
+
 end
