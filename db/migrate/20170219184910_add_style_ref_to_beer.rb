a08a704d1e69af8d005070e9ca3d949db775aaea
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-20 00:09:11 +0200

    Fixing travis tests

--- db/migrate/20170219184910_add_style_ref_to_beer.rb
+++ db/migrate/20170219184910_add_style_ref_to_beer.rb
@@ -1,5 +1,12 @@
 class AddStyleRefToBeer < ActiveRecord::Migration
   def change
+    create_table :styles do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+
     add_reference :beers, :style, index: true, foreign_key: true
     remove_column :styles, :beer_id, :integer
 
